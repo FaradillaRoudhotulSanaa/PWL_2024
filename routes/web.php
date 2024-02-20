19ac7584385f0b1fdab384326b49db658841a698
FaradillaRoudhotulSanaa <faradillaroudhotul@gmail.com>
2024-02-20 11:26:56 +0700

    Update Routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +10,62 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Support\Facades\Route;
+
+//---- BASIC ROUTING ----//
+
+// Langkah B
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+// Langlah D
+Route::get('/world', function () {
+    return 'Wolrd';
+});
+
+// Tugas F
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+// Tugas G 
+Route::get('/about', function () {
+    return '2241720205 - Faradilla Roudhotul S';
+});
+
+//---- ROUTE PARAMETERS ----// 
+
+//Langkah A
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+// Langkah D 
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+// Tugas F 
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+//---- OPERATIONAL PARAMETERS ----//
+
+// Langkah A 
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+// Langkah D 
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+//---- ROUTE NAME ----//
+
+// Langkah A 
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
